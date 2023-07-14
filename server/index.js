4b2d06fd5ba54e53f7ee24987712420648099f1f
Pranjal Goyal <pranjalg984@gmail.com>
2023-07-14 23:19:52 +0530

    Add data base connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from "express";
 import cors from "cors"
-import mongoose from "mongoose"
+import DBConnection from './database/db.js';
 const app=express();
-app.use(express.json);
-app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
+DBConnection();
+//routes
+app.post("/login",(req,res)=>{
+    res.send("My API login")
+})
+app.post("/register",(req,res)=>{
+    res.send("My API register")
+})
+app.get("/" ,(req,res)=>{
+    res.send("my api")
+});
+app.listen(8000, () => {
+    console.log('server is running at port 8000');
+});
