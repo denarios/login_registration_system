0f3d2583b577882df763b76404d12aad16ef99ee
Pranjal Goyal <pranjalg984@gmail.com>
2023-07-14 12:48:42 +0530

    Add an event

--- client/src/component/login/login.js
+++ client/src/component/login/login.js
@@ -1,12 +1,25 @@
-import React from "react"
+import React ,{useState} from "react"
 import "./login.css"
 
 const Login=()=>{
+    const [user,setUser]=useState({
+        email:"",
+        password:""
+    });
+    const handleChange=e=>{
+        const{name,value}=e.target;
+        // console.log(name,value);
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
     return(
         <div className="login">
+        {console.log("user",user)}
         <h1>Login</h1>
-        <input type="text" placeholder="Enter Your Eamil"></input>
-        <input type="password" placeholder="Enter Your password"></input>
+        <input type="text" name="email" value ={user.email} placeholder="Your Eamil" onChange={handleChange}></input>
+        <input type="password" name="password" value={user.password} placeholder="Your password" onChange={handleChange} ></input>
         <div className="button">Login</div>
         <div>or</div>
         <div className="button">Register</div>
--- client/src/component/registration/registration.js
+++ client/src/component/registration/registration.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React,{useState} from "react"
 import "./registration.css"
-
 const Registration=()=>{
+    const [user,setUser]=useState({
+        name:"",
+        email:"",
+        password:"",
+        reEnterpassword:""
+    });
+    const handleChange=e=>{
+        const{name,value}=e.target;
+        console.log(name,value);
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
     return(
         <div className="register">
+         {console.log("user",user)}
         <h1>Register</h1>
-        <input type="text" placeholder="Your Name"></input>
-        <input type="text" placeholder="Your Eamil"></input>
-        <input type="password" placeholder="Your password"></input>
-        <input type="password" placeholder="Re_enter Your password"></input>
+        <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={handleChange} ></input>
+        <input type="text" name="email" value ={user.email} placeholder="Your Eamil" onChange={handleChange}></input>
+        <input type="password" name="password" value={user.password} placeholder="Your password" onChange={handleChange} ></input>
+        <input type="password" name="reEnterpassword" value={user.reEnterpassword} placeholder="Re_enter Your password" onChange={handleChange}></input>
         <div className="button">Register</div>
         <div>or</div>
         <div className="button">Login</div>
